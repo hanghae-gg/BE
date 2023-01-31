79501b32ee9c2fbcd8bc598b8f2fc142d744e9f6
yuminwoo <ymw1023@naver.com>
2023-01-31 20:37:10 +0900

    댓글 response 변경

--- src/main/java/com/hhgg/hhggbe/comment/dto/CommentDto.java
+++ src/main/java/com/hhgg/hhggbe/comment/dto/CommentDto.java
@@ -8,10 +8,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommentDto {
     private Long id;
+    private String username;
     private String comment;
 
     public CommentDto(Comment comment) {
         this.id = comment.getCommentId();
+        this.username = comment.getUser().getUsername();
         this.comment = comment.getComment();
     }
 }
